9d716a8e93d4a4c139a973a2f144b24f9b1fe6e7
AnGaMa666 <pascal2001@live.de>
2023-10-27 12:43:28 +0200

    kommentare

--- main.go
+++ main.go
@@ -1,63 +1,65 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
+	"encoding/json" // Importiert das "encoding/json"-Paket, um JSON-Verarbeitung zu ermöglichen.
+	"log"          // Importiert das "log"-Paket für das Protokollieren von Fehlern.
+	"net/http"      // Importiert das "net/http"-Paket für HTTP-Operationen.
+	"os"           // Importiert das "os"-Paket für Dateioperationen.
+	"strconv"      // Importiert das "strconv"-Paket zur Konvertierung von Zeichenfolgen in Zahlen.
+	"github.com/gin-gonic/gin" // Importiert das "gin"-Paket, um einen HTTP-Server zu erstellen.
 )
 
-const jsonFilePath = "albums.json"
+const jsonFilePath = "albums.json" // Die Konstante jsonFilePath definiert den Dateipfad für die JSON-Datei.
 
 type album struct {
-	ID     string `json:"id"`
+	ID     string `json:"id"`     // Die Struktur "album" repräsentiert ein Musikalbum mit ID, Titel, Künstler und Preis.
 	Title  string `json:"title"`
 	Artist string `json:"artist"`
 	Price  string `json:"price"`
 }
 
+// loadAlbums lädt die Albumdaten aus der JSON-Datei.
 func loadAlbums() []album {
-	file, err := os.Open(jsonFilePath)
+	file, err := os.Open(jsonFilePath) // Öffnet die JSON-Datei zum Lesen.
 	if err != nil {
-		log.Fatal("Fehler beim Öffnen der JSON-Datei:", err)
+		log.Fatal("Fehler beim Öffnen der JSON-Datei:", err) // Protokolliert einen Fehler, falls beim Öffnen ein Fehler auftritt.
 	}
-	defer file.Close()
+	defer file.Close() // Stellt sicher, dass die Datei nach Verlassen der Funktion geschlossen wird.
 
-	decoder := json.NewDecoder(file)
-	var albums []album
+	decoder := json.NewDecoder(file) // Erstellt einen JSON-Decoder, um die Datei zu parsen.
+	var albums []album // Erstellt ein Slice, um die geladenen Alben zu speichern.
 	if err := decoder.Decode(&albums); err != nil {
-		log.Fatal("Fehler beim Parsen der JSON-Datei:", err)
+		log.Fatal("Fehler beim Parsen der JSON-Datei:", err) // Protokolliert einen Fehler, falls beim Parsen ein Fehler auftritt.
 	}
-	return albums
+	return albums // Gibt das Slice der geladenen Alben zurück.
 }
 
+// saveAlbums speichert die Albumdaten in der JSON-Datei.
 func saveAlbums(albums []album) {
-	file, err := os.Create(jsonFilePath)
+	file, err := os.Create(jsonFilePath) // Erstellt die JSON-Datei zum Schreiben.
 	if err != nil {
-		log.Fatal("Fehler beim Erstellen der JSON-Datei:", err)
+		log.Fatal("Fehler beim Erstellen der JSON-Datei:", err) // Protokolliert einen Fehler, falls beim Erstellen ein Fehler auftritt.
 	}
-	defer file.Close()
+	defer file.Close() // Stellt sicher, dass die Datei nach Verlassen der Funktion geschlossen wird.
 
-	encoder := json.NewEncoder(file)
+	encoder := json.NewEncoder(file) // Erstellt einen JSON-Encoder, um die Daten in die Datei zu schreiben.
 	if err := encoder.Encode(albums); err != nil {
-		log.Fatal("Fehler beim Schreiben der Albumdaten in die JSON-Datei:", err)
+		log.Fatal("Fehler beim Schreiben der Albumdaten in die JSON-Datei:", err) // Protokolliert einen Fehler, falls beim Schreiben ein Fehler auftritt.
 	}
 }
 
 func main() {
-	router := gin.Default()
-	router.Use(CORSMiddleware())
+	router := gin.Default() // Erstellt einen neuen HTTP-Router mit den Standardeinstellungen.
+	router.Use(CORSMiddleware()) // Fügt dem Router ein CORS-Middleware hinzu, um Cross-Origin-Anfragen zu verarbeiten.
 
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", createAlbum)
-	router.DELETE("/albums", deleteAlbum)
+	router.GET("/albums", getAlbums) // Definiert einen Endpunkt für das Abrufen von Albumdaten.
+	router.POST("/albums", createAlbum) // Definiert einen Endpunkt zum Erstellen eines neuen Albums.
+	router.DELETE("/albums", deleteAlbum) // Definiert einen Endpunkt zum Löschen von ausgewählten Alben.
 
-	router.Run("localhost:8081")
+	router.Run("localhost:8081") // Startet den HTTP-Server auf Port 8081 auf dem lokalen Host.
 }
 
+// CORSMiddleware fügt CORS-Header zu den Anfragen hinzu, um Cross-Origin-Anfragen zu unterstützen.
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -73,38 +75,40 @@ func CORSMiddleware() gin.HandlerFunc {
 
 		c.Next()
 	}
-
 }
 
+// getAlbums gibt die Liste der Alben als JSON zurück.
 func getAlbums(c *gin.Context) {
-	albums := loadAlbums()
-	c.IndentedJSON(http.StatusOK, albums)
+	albums := loadAlbums() // Lädt die Albumdaten aus der JSON-Datei.
+	c.IndentedJSON(http.StatusOK, albums) // Sendet die Albumdaten als JSON-Antwort zurück.
 }
 
+// createAlbum erstellt ein neues Album und fügt es zur Liste hinzu.
 func createAlbum(c *gin.Context) {
 	var newAlbum album
 	if err := c.ShouldBindJSON(&newAlbum); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) // Sendet eine Fehlerantwort, wenn die Eingabe ungültig ist.
 		return
 	}
 
-	albums := loadAlbums()
-	newAlbum.ID = generateUniqueID(albums)
-	albums = append(albums, newAlbum)
-	saveAlbums(albums)
-	c.JSON(http.StatusCreated, newAlbum)
+	albums := loadAlbums() // Lädt die vorhandenen Alben.
+	newAlbum.ID = generateUniqueID(albums) // Generiert eine eindeutige ID für das neue Album.
+	albums = append(albums, newAlbum) // Fügt das neue Album zur Liste hinzu.
+	saveAlbums(albums) // Speichert die aktualisierte Liste der Alben.
+	c.JSON(http.StatusCreated, newAlbum) // Sendet eine Bestätigungsantwort zurück.
 }
 
+// deleteAlbum löscht ausgewählte Alben aus der Liste.
 func deleteAlbum(c *gin.Context) {
 	var deleteRequest struct {
 		IDs []string `json:"ids"`
 	}
 	if err := c.ShouldBindJSON(&deleteRequest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) // Sendet eine Fehlerantwort, wenn die Eingabe ungültig ist.
 		return
 	}
 
-	albums := loadAlbums()
+	albums := loadAlbums() // Lädt die vorhandenen Alben.
 	idIndex := make(map[string]bool)
 	for _, id := range deleteRequest.IDs {
 		idIndex[id] = true
@@ -112,14 +116,14 @@ func deleteAlbum(c *gin.Context) {
 
 	for i := len(albums) - 1; i >= 0; i-- {
 		if idIndex[albums[i].ID] {
-			albums = append(albums[:i], albums[i+1:]...)
+			albums = append(albums[:i], albums[i+1:]...) // Löscht die ausgewählten Alben aus der Liste.
 		}
 	}
-	saveAlbums(albums)
-
-	c.JSON(http.StatusOK, gin.H{"message": "Selected items deleted"})
+	saveAlbums(albums) // Speichert die aktualisierte Liste der Alben.
+	c.JSON(http.StatusOK, gin.H{"message": "Selected items deleted"}) // Sendet eine Bestätigungsantwort zurück.
 }
 
+// generateUniqueID generiert eine eindeutige ID für ein neues Album.
 func generateUniqueID(albums []album) string {
 	return strconv.Itoa(len(albums) + 1)
 }
--- script.js
+++ script.js
@@ -1,78 +1,125 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Stellt sicher, dass der Code erst ausgeführt wird, wenn die DOM vollständig geladen ist.
+
     const form = document.getElementById("album-form");
+    // Holt das HTML-Formular mit der ID "album-form".
+
     form.addEventListener("submit", function (event) {
+        // Fügt einen "submit"-Event-Listener zum Formular hinzu.
+
         event.preventDefault();
+        // Verhindert das Standardverhalten des Formulars, um die Seite nicht neu zu laden.
+
         const formData = new FormData(form);
+        // Erstellt ein FormData-Objekt aus den Formulardaten.
+
         fetch("http://localhost:8081/albums", {
+            // Sendet eine POST-Anfrage an den Server, um ein neues Album zu erstellen.
             method: "POST",
             body: JSON.stringify(Object.fromEntries(formData)),
+            // Konvertiert die Formulardaten in JSON und sendet sie im Anfragekörper.
             headers: { "Content-Type": "application/json" }
+            // Setzt den Header für JSON-Daten.
         })
         .then(response => response.json())
+        // Parst die JSON-Antwort des Servers.
+
         .then(data => {
+            // Verarbeitet die Antwort des Servers nach dem Erstellen des Albums.
             document.getElementById("response").innerText = "New Album Created: " + JSON.stringify(data);
+            // Zeigt eine Erfolgsmeldung mit den Details des erstellten Albums an.
             form.reset();
+            // Setzt das Formular zurück, um neue Eingaben zu ermöglichen.
+
             // Aktualisiere die Albumliste nach dem Hinzufügen
             updateAlbumList();
-            
         })
         .catch(error => console.error(error));
+        // Zeigt Fehlermeldungen in der Konsole an, wenn etwas schief geht.
     });
 
     const showListButton = document.getElementById("show-list");
+    // Holt den HTML-Button mit der ID "show-list".
+
     showListButton.addEventListener("click", function (event) {
+        // Fügt einen "click"-Event-Listener zum Button hinzu.
+
         event.preventDefault();
+        // Verhindert das Standardverhalten des Buttons, um die Seite nicht neu zu laden.
+
         // Aktualisiere die Albumliste, ohne die DELETE-Anfrage auszulösen
         updateAlbumList();
+        // Ruft die Funktion auf, um die Liste der Alben anzuzeigen.
     });
 
     const deleteButton = document.getElementById("delete-button");
+    // Holt den HTML-Button mit der ID "delete-button".
+
     deleteButton.addEventListener("click", function (event) {
+        // Fügt einen "click"-Event-Listener zum Button hinzu.
+
         event.preventDefault();
+        // Verhindert das Standardverhalten des Buttons, um die Seite nicht neu zu laden.
 
         const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        // Sammelt alle ausgewählten Checkboxen, die zum Löschen markiert sind.
 
         if (checkboxes.length === 0) {
-            // Keine Checkboxen ausgewählt, also nicht löschen
+            // Überprüft, ob mindestens eine Checkbox ausgewählt ist.
             alert("Please select at least one item to delete.");
+            // Zeigt eine Warnung, wenn keine Checkbox ausgewählt ist.
             return;
         }
 
         const selectedAlbumIds = Array.from(checkboxes).map(checkbox => checkbox.getAttribute("data-id"));
+        // Extrahiert die IDs der ausgewählten Alben.
 
         fetch("http://localhost:8081/albums", {
             method: "DELETE",
+            // Sendet eine DELETE-Anfrage an den Server, um ausgewählte Alben zu löschen.
             headers: { "Content-Type": "application/json" },
+            // Setzt den Header für JSON-Daten.
             body: JSON.stringify({ ids: selectedAlbumIds })
+            // Konvertiert die ausgewählten IDs in JSON und sendet sie im Anfragekörper.
         })
         .then(response => {
             if (response.status === 200) {
                 console.log("Selected items deleted");
+                // Zeigt eine Erfolgsmeldung in der Konsole an.
                 // Aktualisiere die Albumliste nach dem Löschen
                 updateAlbumList();
             } else if (response.status === 404) {
                 console.error("One or more items not found");
+                // Zeigt eine Fehlermeldung in der Konsole an, wenn Alben nicht gefunden wurden.
             } else {
                 console.error("Error deleting selected items");
+                // Zeigt eine allgemeine Fehlermeldung in der Konsole an.
             }
         })
         .catch(error => console.error(error));
+        // Zeigt Fehlermeldungen in der Konsole an, wenn etwas schief geht.
     });
 
     // Funktion zum Aktualisieren der Albumliste
     function updateAlbumList() {
         fetch("http://localhost:8081/albums", {
+            // Sendet eine GET-Anfrage an den Server, um die Liste der Alben abzurufen.
             method: "GET",
             headers: { "Content-Type": "application/json" }
+            // Setzt den Header für JSON-Daten.
         })
         .then(response => response.json())
+        // Parst die JSON-Antwort des Servers.
+
         .then(data => {
+            // Verarbeitet die Antwort des Servers nach dem Aktualisieren der Liste.
             const formattedData = data.map(item => ({
                 title: item.title,
                 artist: item.artist,
                 price: parseFloat(item.price).toLocaleString("de-DE", { style: "currency", currency: "EUR" }),
                 id: item.id
             }));
+            // Formatiert die Alben-Daten für die Anzeige.
 
             const table = `
                 <table>
@@ -94,11 +141,15 @@ document.addEventListener("DOMContentLoaded", function () {
                     `).join('')}
                 </table>
             `;
+            // Erstellt eine HTML-Tabelle mit den Alben-Daten.
 
             const jsonResponse = document.getElementById("json-response");
             jsonResponse.innerHTML = table;
+            // Aktualisiert den Inhalt des HTML-Elements mit der ID "json-response".
             jsonResponse.style.textAlign = "center";
+            // Zentriert den Text in diesem Element.
         })
         .catch(error => console.error(error));
+        // Zeigt Fehlermeldungen in der Konsole an, wenn etwas schief geht.
     }
 });
